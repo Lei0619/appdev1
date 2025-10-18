9aa25f79123dab8a21130e54eb45ad2db226d538
Hood <97504681+Hood@users.noreply.github.com>
2025-10-18 17:32:43 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,10 @@
+let grade = 70;
+if (grade >= 90) {
+    console.log('A');
+} else if (grade >= 80) {
+    console.log('B');
+} else if (grade >= 70) {
+    console.log('C');
+} else {
+    console.log('F');
+}
