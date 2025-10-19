08e7b7a1b948c266c744b6e524e8565a18b71b7d
Hood <97504681+Hood@users.noreply.github.com>
2025-10-19 21:19:41 +0800

    Done sharing data between components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -83,3 +83,24 @@ export default function App() {
     </>
   );
 }
+
+import { useState } from 'react';
+
+function Button({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <Button count={count} onClick={handleClick} />
+      <Button count={count} onClick={handleClick} />
+    </>
+  );
+}
