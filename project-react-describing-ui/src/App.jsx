8605ceda48f0732139613321ba5eb5a3edd8b200
Hood <97504681+Hood@users.noreply.github.com>
2025-10-25 13:11:39 +0800

    Done with how to configure components with props

--- project-react-describing-ui/src/App.jsx
+++ project-react-describing-ui/src/App.jsx
@@ -1,12 +1,16 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Greeting from './components/Greeting';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <>
       <Header />
       <Greeting />
+      <Welcome user="Lei" />
+      <Welcome user="Alex" />
+      <Welcome user="Kai" />
       <p>Main content goes here.</p>
       <div>
         <h1>React with JSX</h1>
