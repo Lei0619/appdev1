7812f20ac470181f13f506dee703f6215f4ea88b
Hood <97504681+Hood@users.noreply.github.com>
2025-11-09 22:45:29 +0800

    Done with the root component file

---
+++ react-importing-exporting-components/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Header';
+import { Footer, Signature } from './Footer';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Signature />
+    </div>
+  );
+}
---
+++ react-importing-exporting-components/src/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return <p>2025 My React App</p>;
+}
+
+export function Signature() {
+  return <p>Lei</p>;
+}
