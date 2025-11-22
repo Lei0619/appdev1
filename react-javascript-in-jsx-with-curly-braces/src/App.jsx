80854f4c4aaacd559f8962cd00e4070fdbd66d38
Hood <97504681+Hood@users.noreply.github.com>
2025-11-22 10:26:36 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -7,3 +7,22 @@ export default function Avatar() {
     />
   );
 }
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Dico Lei Ann Judea"
+      />
+      <ul>
+        <li>Implement Testing</li>
+        <li>Finish Deliverables</li>
+        <li>Work</li>
+      </ul>
+    </div>
+  );
+}
+
